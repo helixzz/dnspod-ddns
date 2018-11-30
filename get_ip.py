cd05e1cbb6208cb72bd4855ed1838e1a302bd90d
HeliXZz <helixzz@live.cn>
2018-11-30 17:00:01 +0800

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -3,7 +3,6 @@ import logging
 import socket
 from urllib import request, error, parse
 
-# 匹配合法 IP 地址
 regex_ip = re.compile(
     r"\D*("
     + r"(?:1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])\."
@@ -12,7 +11,6 @@ regex_ip = re.compile(
     + r"(?:1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)"
     + r")\D*")
 
-# 增强鲁棒性，用多种方式获取 IP
 def get_ip():
     return (get_ip_by_azure())
     
@@ -25,6 +23,5 @@ def get_ip_by_azure():
         logging.warning("get_ip_by_ipip FAILED, error: %s", str(e))
         return None
  
-# 测试
 if __name__ == '__main__':
-    print(get_ip()                     )
+    print(get_ip())
