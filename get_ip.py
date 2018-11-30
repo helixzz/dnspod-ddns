339e452d2d8348f0b3d5f96df560cb4f9b5b344f
HeliXZz <helixzz@live.cn>
2018-11-30 16:59:20 +0800

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -14,56 +14,17 @@ regex_ip = re.compile(
 
 # 增强鲁棒性，用多种方式获取 IP
 def get_ip():
-    return (get_ip_by_ipip()
-        or  get_ip_by_httpbin()
-        or  get_ip_by_httpbin_direct_1()
-        or  get_ip_by_httpbin_direct_2() )
+    return (get_ip_by_azure())
     
-# 这几个函数会在 DNS 遭受污染时失效
-def get_ip_by_ipip():
-    url = 'http://myip.ipip.net/'
+def get_ip_by_azure():
+    url = 'http://42.159.252.221:55555/'
     try:
         resp = request.urlopen(url=url, timeout=10).read()
         return regex_ip.match(resp.decode("utf-8")).group(1)
     except Exception as e:
         logging.warning("get_ip_by_ipip FAILED, error: %s", str(e))
         return None
-
-def get_ip_by_httpbin():
-    url = 'http://www.httpbin.org/ip'
-    try:
-        resp = request.urlopen(url=url, timeout=10).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
-    except Exception as e:
-        logging.warning("get_ip_by_httpbin FAILED, error: %s", str(e))
-        return None
-
-# 这个函数可以在本地 DNS 遭受污染的时候获取到IP
-# 如需模拟DNS污染，可以在HOSTS文件里加入 127.0.0.1 www.httpbin.org
-def get_ip_by_httpbin_direct_1():
-    url = 'http://52.5.182.176/ip'
-    try:
-        req = request.Request(url=url, method='GET', headers={'Host': 'www.httpbin.org'})
-        resp = request.urlopen(req).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
-    except Exception as e:
-        logging.warning("get_ip_by_httpbin_direct_1 FAILED, error: %s", str(e))
-        return None
-
-def get_ip_by_httpbin_direct_2():
-    url = 'http://52.44.230.61/ip'
-    try:
-        req = request.Request(url=url, method='GET', headers={'Host': 'www.httpbin.org'})
-        resp = request.urlopen(req).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
-    except Exception as e:
-        logging.warning("get_ip_by_httpbin_direct_2 FAILED, error: %s", str(e))
-        return None
-
-    
+ 
 # 测试
 if __name__ == '__main__':
     print(get_ip()                     )
-    print(get_ip_by_ipip()             )
-    print(get_ip_by_httpbin()          )
-    print(get_ip_by_httpbin_direct_1() )
